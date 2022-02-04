3ed6ff8ae0b5e5898e4c83def4632b8556f4a37d
Tomasz Cieślar <cieslar.tomek@gmail.com>
2022-02-04 20:31:56 +0100

    add validates for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  validates_presence_of :name, :time, :capacity
+  validate :capacity_gater_than_zero
+
   has_many :tickets
+
+  def capacity_gater_than_zero
+    errors.add(:capacity, "must be grater then 0") if capacity < 1
+  end
 
   def taken_tickets_count
     tickets.select { |ticket| ["reserved", "confirmed"].include?(ticket.status) }.sum(&:quantity)
